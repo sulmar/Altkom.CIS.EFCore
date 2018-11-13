3705c9169b401afc62d50b33db7d42c83cc165a3
Marcin Sulecki <marcin.sulecki@gmail.com>
2018-11-13 15:45:43 +0100

    day 1

---
+++ Altkom.CIS.EFCore.SampleData/Fakers/OrderDetailFaker.cs
@@ -0,0 +1,21 @@
+﻿using Altkom.CIS.EFCore.Models;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altkom.CIS.EFCore.SampleData.Fakers
+{
+    class OrderDetailFaker : Faker<OrderDetail>
+    {
+        public OrderDetailFaker(IList<Item> items)
+        {
+            StrictMode(true);
+            Ignore(p => p.Id);
+            RuleFor(p => p.Item, f => f.PickRandom(items));
+            RuleFor(p => p.Quantity, f => f.Random.Number(1, 10));
+            RuleFor(p => p.UnitPrice, (f, d) => d.Item.UnitPrice);
+            FinishWith((f, d) => Console.WriteLine($"Created detail {d.Item.Name} {d.Quantity} {d.UnitPrice}"));
+        }
+    }
+}
---
+++ Altkom.CIS.EFCore.SampleData/Fakers/OrderFaker.cs
@@ -0,0 +1,24 @@
+﻿using Altkom.CIS.EFCore.Models;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altkom.CIS.EFCore.SampleData.Fakers
+{
+    class OrderFaker : Faker<Order>
+    {
+        public OrderFaker(IList<Customer> customers, OrderDetailFaker orderDetailFaker)
+        {
+            StrictMode(true);
+            Ignore(p => p.Id);
+            RuleFor(p => p.OrderNumber, f => f.Random.Replace("###-###-###"));
+            RuleFor(p => p.OrderDate, f => f.Date.Past(3));
+            RuleFor(p => p.Customer, f => f.PickRandom(customers));
+            RuleFor(p => p.Details, f => orderDetailFaker.Generate(f.Random.Number(1, 5)));
+            RuleFor(p => p.DeliveryDate, (f, order) => order.OrderDate.AddDays(f.Random.Number(2, 14)));
+            FinishWith((f, order) => Console.WriteLine($"Created order {order.OrderNumber}"));
+
+        }
+    }
+}
